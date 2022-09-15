0e780edc81dabd82f20c41ea8b3291d2341b4b01
Nandhini-03 <108286273+Nandhini-03@users.noreply.github.com>
2022-09-15 14:10:36 +0530

    Update 2.Remove duplicates from sorted array.java

--- Microsoft/In-Class Assignments/Week-1/2.Remove duplicates from sorted array.java
+++ Microsoft/In-Class Assignments/Week-1/2.Remove duplicates from sorted array.java
@@ -1,1 +1,17 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int c=1;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i-1]<nums[i])
+            {
+                nums[c]=nums[i];
+                c++;
+            }
+        }
+        return c;
+    }
+}
 
+/*Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_] */
