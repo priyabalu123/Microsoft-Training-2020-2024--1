e240d4686cf72ffd1f8e99d68d0e0fd4f9065b53
Nandhini-03 <108286273+Nandhini-03@users.noreply.github.com>
2022-09-15 20:14:06 +0530

    Create 5.Area of circle.java

---
+++ Google Assignments/Assignment-1/5.Area of circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int N=sc.nextInt();
+    double area=Math.PI*N*N;
+    System.out.print(area);
+  }
+}
+
+/*Input:5
+ Output:78.55 */
