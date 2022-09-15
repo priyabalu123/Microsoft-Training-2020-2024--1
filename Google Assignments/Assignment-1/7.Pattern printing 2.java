120d0e550ea3df26eedb615b555d86371379fe0c
Nandhini-03 <108286273+Nandhini-03@users.noreply.github.com>
2022-09-15 21:27:23 +0530

    Update 7.Pattern printing 2.java

--- Google Assignments/Assignment-1/7.Pattern printing 2.java
+++ Google Assignments/Assignment-1/7.Pattern printing 2.java
@@ -1,1 +1,23 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int N=sc.nextInt();
+    for(int i=N;i>=1;i--)
+      {
+        for(int j=i;j>=1;j--)
+          {
+            System.out.print(j+" ");
+          }
+        System.out.println();
+      }
+  }
+}
 
+/*Input:5
+  Output:
+  5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1
+1 */
