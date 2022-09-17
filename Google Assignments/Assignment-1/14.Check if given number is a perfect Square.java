2c5cc2559f55e74e7a50fad92304d3a2ade53933
Nandhini-03 <108286273+Nandhini-03@users.noreply.github.com>
2022-09-17 14:56:57 +0530

    Update 14.Check if given number is a perfect Square.java

--- Google Assignments/Assignment-1/14.Check if given number is a perfect Square.java
+++ Google Assignments/Assignment-1/14.Check if given number is a perfect Square.java
@@ -1,1 +1,18 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int N=sc.nextInt();
+    double sq=Math.sqrt(N);
+    if(sq==Math.floor(sq))
+    {
+      System.out.print("Perfect Square");
+    }
+    else{
+      System.out.print("Not a perfect Square");
+    }
+  }
+}
 
+/*Input:25
+ Output:Perfect Square */
