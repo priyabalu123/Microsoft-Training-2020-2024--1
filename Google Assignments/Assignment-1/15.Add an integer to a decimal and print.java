99bf01560c46327429f17b6c87008a054f9fc0a1
Nandhini-03 <108286273+Nandhini-03@users.noreply.github.com>
2022-09-17 15:06:13 +0530

    Update 15.Add an integer to a decimal and print.java

--- Google Assignments/Assignment-1/15.Add an integer to a decimal and print.java
+++ Google Assignments/Assignment-1/15.Add an integer to a decimal and print.java
@@ -1,1 +1,13 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int X=sc.nextInt();
+    float Y=sc.nextFloat();
+    double Z=X+Y;
+    System.out.print(Z);
+  }
+}
 
+/*Input:3 4.7
+  Output:7.699999809265137 */
