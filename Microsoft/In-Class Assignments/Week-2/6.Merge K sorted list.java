f5852e5653fa2425d377a4e263bd5a4aad2c776e
priyabalu123 <108317247+priyabalu123@users.noreply.github.com>
2022-09-17 20:41:44 +0530

    Update 6.Merge K sorted list.java

--- Microsoft/In-Class Assignments/Week-2/6.Merge K sorted list.java
+++ Microsoft/In-Class Assignments/Week-2/6.Merge K sorted list.java
@@ -1,35 +1,28 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length==0)
-            return null;
-        int interval=1;
-        while(interval<lists.length)
+        if(lists == null || lists.length==0)
         {
-            system.out.println(lists.length);
-            for(int i=0;i+interval<lists.length;i=i+interval*2)
-            {
-                lists[i]=mergeTwoLists(list[i],lists[i+interval]);
-            }
-            interval*=2;
+            return null;
         }
-        return list[0];
-    }
-}
-public ListNode mergeTwoLists(ListNode l1,ListNode l2)
-{
-    if(l1==null)
-        return l2;
-    if(l2==null)
-        return l1;
-    if(l1.val<l2.val)
-    {
-        l1.next=mergeTwoLists(l1.next,l2);
-        return l1;
-    }
-    else
-    {
-        l2.next=mergeTwoLists(l1,l2.next);
-        return l2;
+        ListNode head=new ListNode(0);
+        ListNode temp=head;
+        while(true)
+        {
+            int p=0;
+            for(int i=0;i<lists.length;i++)
+            {
+                if(lists[p]==null || lists[i]!=null && lists[p].val > lists[i].val)
+                    p=i;
+            }
+            if(lists[p]==null)
+            {
+                break;
+            }
+            temp.next=lists[p];
+            temp=temp.next;
+            lists[p]=lists[p].next;
+        }
+        return head.next;
     }
 }
 
